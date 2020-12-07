4db0a8f16a1dfc2c944896ff079913d9b53c1705
zaenal <zaenal@laku6.com>
2020-12-07 22:12:49 +0700

    v2-retrofit-setup: setup response, and retrofit instance

---
+++ app/src/main/java/com/zaen/mvvmnewsapppractice/Source.kt
@@ -0,0 +1,6 @@
+package com.zaen.mvvmnewsapppractice
+
+data class Source(
+    val id: Any,
+    val name: String
+)
---
+++ app/src/main/java/com/zaen/mvvmnewsapppractice/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.zaen.mvvmnewsapppractice.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "3a9954a573934d4d90076ff08d22e6b5"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
