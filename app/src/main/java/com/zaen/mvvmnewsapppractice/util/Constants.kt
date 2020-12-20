a37e4b83e49501f7a0d4fb34032aba333778cff9
zaenal <zaenal@laku6.com>
2020-12-20 13:37:12 +0700

    v8: add handling searching news

--- app/src/main/java/com/zaen/mvvmnewsapppractice/util/Constants.kt
+++ app/src/main/java/com/zaen/mvvmnewsapppractice/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "3a9954a573934d4d90076ff08d22e6b5"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
